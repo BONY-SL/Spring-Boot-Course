3799855d4c6d05fd34cab11038133f348330c3b5
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-08-10 21:56:55 +0530

    update

--- Spring Boot Course/Spring-Boot-Course/src/main/java/com/springbootcourse/Spring/Boot/Course/ApplicationConfig.java
+++ Spring Boot Course/Spring-Boot-Course/src/main/java/com/springbootcourse/Spring/Boot/Course/ApplicationConfig.java
@@ -4,18 +4,22 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 
 @Configuration
+//@Profile("dev")
 public class ApplicationConfig {
 
 
     @Bean
+    //@Profile("dev")
     public MyFirstClass myFirstBean(){
       return new MyFirstClass("  first Bean");
    }
 
     @Bean
+    //@Profile("test")
     public MyFirstClass mySecondBean(){
         return new MyFirstClass("  second Bean");
     }
--- Spring Boot Course/Spring-Boot-Course/src/main/java/com/springbootcourse/Spring/Boot/Course/SpringBootCourseApplication.java
+++ Spring Boot Course/Spring-Boot-Course/src/main/java/com/springbootcourse/Spring/Boot/Course/SpringBootCourseApplication.java
@@ -4,12 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class SpringBootCourseApplication {
 
 	public static void main(String[] args) {
 
-		var context= SpringApplication.run(SpringBootCourseApplication.class, args);
+
+		var app = new SpringApplication(SpringBootCourseApplication.class);
+
+		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active","dev"));
+
+		var context= app.run(args);
+
+//		var app = new SpringApplication(SpringBootCourseApplication.class);
+//
+//		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active","dev"));
+//
+//		var context= app.run(args);
 
 		MyFirstService myFirstService = context.getBean(MyFirstService.class);
 		System.out.println(myFirstService.tellAStory());
