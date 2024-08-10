56c4d3dcb17d7054e004fe012bc8cb340bec1d17
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-08-10 23:35:14 +0530

    clean

--- Spring Boot Course/Spring-Boot-Course/src/main/java/com/springbootcourse/Spring/Boot/Course/SpringBootCourseApplication.java
+++ Spring Boot Course/Spring-Boot-Course/src/main/java/com/springbootcourse/Spring/Boot/Course/SpringBootCourseApplication.java
@@ -1,31 +1,13 @@
 package com.springbootcourse.Spring.Boot.Course;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.util.Collections;
 
 @SpringBootApplication
 public class SpringBootCourseApplication {
 
 	public static void main(String[] args) {
 
-
-		var app = new SpringApplication(SpringBootCourseApplication.class);
-
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active","dev"));
-
-		var context= app.run(args);
-
-//		var app = new SpringApplication(SpringBootCourseApplication.class);
-//
-//		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active","dev"));
-//
-//		var context= app.run(args);
-
-		MyFirstService myFirstService = context.getBean(MyFirstService.class);
-		System.out.println(myFirstService.tellAStory());
-
+		SpringApplication.run(SpringBootCourseApplication.class);
 	}
 }
