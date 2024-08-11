f8dab730654238ec0643c06b5a62c9146e1a80eb
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-08-12 00:17:32 +0530

    update

--- Spring Boot Course/Spring-Boot-Course/src/main/java/com/springbootcourse/Spring/Boot/Course/FirstController.java
+++ Spring Boot Course/Spring-Boot-Course/src/main/java/com/springbootcourse/Spring/Boot/Course/FirstController.java
@@ -11,36 +11,9 @@ public class FirstController {
         return "Hello World....";
     }
 
-
     @PostMapping("/post")
     public String post(@RequestParam (value = "message") String message){
 
         return "Request accepted..." +message;
-    }
-
-    @PostMapping("/post-order")
-    public String postOrder(@RequestBody Order order){
-
-        return "Request accepted..." + order.toString();
-    }
-
-    @PostMapping("/post-order-record")
-    public String postOrderRecord(@RequestBody OrderRecord order){
-
-        return "Request accepted..." + order.toString();
-    }
-
-    //http://localhost:8080/hellodilshan/dilshan
-    @GetMapping("/hellodilshan")
-    public String requestParam(@RequestParam("name") String  name){
-
-        return "Request accepted... RequestParam " + name;
-    }
-
-    //http://localhost:8080/hellodilshan?name=dilshan
-    @GetMapping("/hellodilshan2/{name}")
-    public String pathVariable(@PathVariable("name") String  name){
-
-        return "Request accepted...PathVariable " + name;
     }
 }
--- Spring Boot Course/Spring-Boot-Course/src/main/java/com/springbootcourse/Spring/Boot/Course/OrderRecord.java
+++
@@ -1,8 +0,0 @@
-package com.springbootcourse.Spring.Boot.Course;
-
-public record OrderRecord(
-        String productName,
-        String customerName,
-        int quntity
-) {
-}
