60d17d50e0f545c2fd7bb739467d12b91c4535c7
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-08-11 23:22:12 +0530

    update

--- Spring Boot Course/Spring-Boot-Course/src/main/java/com/springbootcourse/Spring/Boot/Course/FirstController.java
+++ Spring Boot Course/Spring-Boot-Course/src/main/java/com/springbootcourse/Spring/Boot/Course/FirstController.java
@@ -23,4 +23,24 @@ public class FirstController {
 
         return "Request accepted..." + order.toString();
     }
+
+    @PostMapping("/post-order-record")
+    public String postOrderRecord(@RequestBody OrderRecord order){
+
+        return "Request accepted..." + order.toString();
+    }
+
+    //http://localhost:8080/hellodilshan/dilshan
+    @GetMapping("/hellodilshan")
+    public String requestParam(@RequestParam("name") String  name){
+
+        return "Request accepted... RequestParam " + name;
+    }
+
+    //http://localhost:8080/hellodilshan?name=dilshan
+    @GetMapping("/hellodilshan2/{name}")
+    public String pathVariable(@PathVariable("name") String  name){
+
+        return "Request accepted...PathVariable " + name;
+    }
 }
---
+++ Spring Boot Course/Spring-Boot-Course/src/main/java/com/springbootcourse/Spring/Boot/Course/OrderRecord.java
@@ -0,0 +1,8 @@
+package com.springbootcourse.Spring.Boot.Course;
+
+public record OrderRecord(
+        String productName,
+        String customerName,
+        int quntity
+) {
+}
